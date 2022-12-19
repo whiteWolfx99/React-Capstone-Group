32e1b684cb4a1f4a5a8e2a43662fce9437782d4a
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-19 22:46:46 +0300

    finish group tasks

--- src/Views/Navbar.js
+++ src/Views/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import planet from '../Assets/Images/planet.png';
 
 const Navbar = () => (
-  <div>
-    <Link to="/">Rockets</Link>
-    <Link to="/missions">Missions</Link>
-    <Link to="/myprofile">My Profile</Link>
+  <div className="navbar">
+    <div className="logo">
+      <img src={planet} alt="planet" />
+      <h1>Space Travelers&apos; Hub</h1>
+    </div>
+    <div className="nav-links">
+      <Link to="/">Rockets</Link>
+      <Link className="link-border" to="/missions">Missions</Link>
+      <Link to="/myprofile">My Profile</Link>
+    </div>
   </div>
 );
 
