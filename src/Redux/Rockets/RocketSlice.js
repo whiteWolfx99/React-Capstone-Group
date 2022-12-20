0b2b41f387de6270e282791e4e3f33caa913e4ac
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2022-12-20 18:31:43 +0300

    setup redux

---
+++ src/Redux/Rockets/RocketSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const RocketSlice = createSlice({
+  name: 'rockets',
+  initialState: [],
+  reducers: {},
+  extraReducers: () => {
+  },
+});
+
+export default RocketSlice.reducer;
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsSlice from './missions/missions';
+import RocketSlice from './Rockets/RocketSlice';
 
 export default configureStore({
   reducer: {
     missions: missionsSlice,
+    rockets: RocketSlice,
   },
 });
