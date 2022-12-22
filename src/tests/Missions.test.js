049cc79ab1956e16eefcd24a9900b40a337dfb0c
vanelnw <vanelnwaba@gmail.com>
2022-12-22 18:50:49 +0100

    mission test

---
+++ src/tests/Missions.test.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Missions from "../Views/Missions";
+import { Provider } from "react-redux";
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const mockStore = configureStore([thunk]);
+
+const mockMissions = [
+  {
+    id: 1,
+    name: "Mission 1",
+    description: "This is the first mission",
+    reserved: false,
+  },
+  {
+    id: 2,
+    name: "Mission 2",
+    description: "This is the second mission",
+    reserved: true,
+  },
+  {
+    id: 3,
+    name: "Mission 3",
+    description: "This is the third mission",
+    reserved: false,
+  },
+];
+
+const store = mockStore({
+  missions: mockMissions,
+});
+
+describe("Missions component", () => {
+  it("renders the missions table correctly", () => {
+    const { getByText, } = render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>
+    );
+
+    mockMissions.forEach((mission) => {
+      const missionNameElement = getByText(mission.name);
+      expect(missionNameElement).toBeDefined();
+      const missionDescriptionElement = getByText(mission.description);
+      expect(missionDescriptionElement).toBeDefined();
+    });
+  });
+
+  it("dispatches the joinOrLeaveMission action when the button is clicked", async () => {
+    const { getAllByText } = render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>
+    );
+
+    const joinButton = getAllByText("Join Mission");
+    expect(joinButton).not.toBeNull();
+
+  });
+});
