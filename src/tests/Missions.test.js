ed8949b291a5939835456c93ec3215d6c457f45e
vanelnw <vanelnwaba@gmail.com>
2022-12-22 18:53:18 +0100

    linters

--- src/tests/Missions.test.js
+++ src/tests/Missions.test.js
@@ -1,29 +1,29 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Missions from "../Views/Missions";
-import { Provider } from "react-redux";
-import configureStore from "redux-mock-store";
-import thunk from "redux-thunk";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import Missions from '../Views/Missions';
 
 const mockStore = configureStore([thunk]);
 
 const mockMissions = [
   {
     id: 1,
-    name: "Mission 1",
-    description: "This is the first mission",
+    name: 'Mission 1',
+    description: 'This is the first mission',
     reserved: false,
   },
   {
     id: 2,
-    name: "Mission 2",
-    description: "This is the second mission",
+    name: 'Mission 2',
+    description: 'This is the second mission',
     reserved: true,
   },
   {
     id: 3,
-    name: "Mission 3",
-    description: "This is the third mission",
+    name: 'Mission 3',
+    description: 'This is the third mission',
     reserved: false,
   },
 ];
@@ -32,12 +32,12 @@ const store = mockStore({
   missions: mockMissions,
 });
 
-describe("Missions component", () => {
-  it("renders the missions table correctly", () => {
-    const { getByText, } = render(
+describe('Missions component', () => {
+  it('renders the missions table correctly', () => {
+    const { getByText } = render(
       <Provider store={store}>
         <Missions />
-      </Provider>
+      </Provider>,
     );
 
     mockMissions.forEach((mission) => {
@@ -48,15 +48,14 @@ describe("Missions component", () => {
     });
   });
 
-  it("dispatches the joinOrLeaveMission action when the button is clicked", async () => {
+  it('dispatches the joinOrLeaveMission action when the button is clicked', async () => {
     const { getAllByText } = render(
       <Provider store={store}>
         <Missions />
-      </Provider>
+      </Provider>,
     );
 
-    const joinButton = getAllByText("Join Mission");
+    const joinButton = getAllByText('Join Mission');
     expect(joinButton).not.toBeNull();
-
   });
 });
